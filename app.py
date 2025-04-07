c16dc441e8858430985c547f6424f71a102a1a39
Rumana Khanam <aakhi.zaman@gmail.com>
2025-04-06 20:27:33 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from Khanam ECS Container."
+    return (
+        "Hello from Khanam ECS Container.<br>"
+        "New line added to test CodePipeline trigger.<br>"
+    )
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
